025efaa0bb2d066d0d698e76dad51e20ad19c04c
Himanshu Rawat <himanshurawat90k@gmail.com>
2023-01-29 20:06:53 +0530

    add Tadk to todos array.

--- src/App.js
+++ src/App.js
@@ -1,13 +1,35 @@
-import { useState } from 'react';
+import { useState, useReducer } from 'react';
+
+const ACTIONS = {
+  ADD_TASK: "add_task"
+};
+
+const reducer = (todos, action) => {
+  switch (action.types) {
+    case ACTIONS.ADD_TASK:
+      return [...todos, addTask(action.payload.task)];
+    default:
+      return todos;
+  };
+};
+
+const addTask = (taskName) => {
+  return {id: Date.now(), complete: false, task: taskName};
+};
 
 const App = () => {
 
   const [taskName, setTaskName] = useState("");
 
+  const [todos, dispatch] = useReducer(reducer, []);
+
   const handleSubmit = (e) => {
     e.preventDefault();
+    dispatch({types: ACTIONS.ADD_TASK, payload: {task: taskName}});
     setTaskName("");
   };
+
+  console.log(todos);
 
   return(
     <>
