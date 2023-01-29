a00d6ea377908ef04c664710e90d4b2b1bf2a21d
Himanshu Rawat <himanshurawat90k@gmail.com>
2023-01-29 19:17:01 +0530

    Entered form

--- src/App.js
+++ src/App.js
@@ -1,55 +1,9 @@
-import { useState, useReducer } from 'react';
-import Todo from './Todo';
-
-export const ACTIONS = {
-  ADD_TODO: "add_todo",
-  TOGGLE_TODO: "toggle_todo",
-  DELETE_TODO: "delete_todo"
-};
-
-const reducer = (todos, action) => {
-  switch (action.type) {
-    case ACTIONS.ADD_TODO:
-      return ([...todos, addTask(action.payload.name)]);
-    // case ACTIONS.TOGGLE_TODO:
-    //   return todos.map(todo => {
-    //     if(todo.id === action.payload.id) {
-    //       return {...todo, complete: !todo.complete};
-    //     }
-    //     return todo; 
-    //   })
-    default:
-      return todos;
-  };
-};
-
-const addTask = (name) => {
-  return ({id: Date.now(), name: name, complete: false});
-};
-
 const App = () => {
-
-  const [name, setName] = useState("");
-
-  const [todos, dispatch] = useReducer(reducer, []);
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    dispatch({type: ACTIONS.ADD_TODO, payload: {name: name}});
-    setName("");
-  }; 
-
-  return (
+  return(
     <>
-      <form onSubmit={handleSubmit}>
-        <input type="text" value={name} onChange={e => setName(e.target.value)} />
-      </form> 
-
-      {
-        todos.map((todo) => {
-          return <Todo key={todo.id} todo={todo} dispatch={dispatch}/>
-        })
-      }   
+      <form>
+        <input type="text" value="Enter Task"/>
+      </form>
     </>
   );
 };
