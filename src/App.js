b602c7dd90aae722fc9e5293806c9cd07d383353
Himanshu Rawat <himanshurawat90k@gmail.com>
2023-01-29 19:21:55 +0530

    made input interactive

--- src/App.js
+++ src/App.js
@@ -1,8 +1,13 @@
+import { useState } from 'react';
+
 const App = () => {
+
+  const [taskName, setTaskName] = useState("Enter Task");
+
   return(
     <>
       <form>
-        <input type="text" value="Enter Task"/>
+        <input type="text" value={taskName} onChange={(e) => setTaskName(e.target.value)}/>
       </form>
     </>
   );
